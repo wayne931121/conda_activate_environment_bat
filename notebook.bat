8cd3868edb6468a7cab67d9acb6ba589a4fb4cc5
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2025-10-13 18:46:14 +0800

    Update notebook.bat

--- notebook.bat
+++ notebook.bat
@@ -5,5 +5,9 @@ rem idk why chcp 65001 need run after conda activate, or it will activate failed
 chcp 65001
 set USE_LIBUV=0
 rem set KMP_DUPLICATE_LIB_OK=TRUE
+rem wayne931121_jupyter_notebook_config.py in https://github.com/wayne931121/conda_activate_environment_bat/tree/main/attachment
+rem my env in C://ai, need administrator, but jupyter_notebook_config.py in "C:\Users\原神\Downloads\wayne931121_jupyter_notebook_config.py"
+rem so I do some change in command for all user.
 jupyter notebook --config="C:\Users\原神\Downloads\wayne931121_jupyter_notebook_config.py"
+
 cmd
